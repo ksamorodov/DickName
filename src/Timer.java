50214cc1eb729a19df702676ec6efcde9b2d6f2c
ksamorodov <ksamorodov@yandex.ru>
2022-11-10 19:17:57 +0300

    Initial commit

---
+++ src/Timer.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Timer {
+    public static final String PREFIX = "Хуи";
+    public static final String PREFIX_EXCEPTION = "Хуй";
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Введите свое имя: ");
+            Scanner in = new Scanner(System.in);
+            System.out.println(createDickName(in.next()));
+        }
+    }
+
+    public static String createDickName(String name) {
+        name = name.toLowerCase();
+
+        boolean isFirstLetterVowel = name.matches("^(?ui:[аеёиоуыэюя]).*");
+        boolean isFirstLetterVowelCompositePart = name.matches("^(?ui:[эоуа]).*");
+        boolean isFirstLetterVowelComposite = name.matches("^(?ui:[еёюя]).*");
+
+        boolean isSecondLetterVowel = name.matches("^(?ui:.[аиоуыэеёюя]).*");
+        boolean isSecondLetterShortI = name.matches("^(?ui:.[й]*)");
+        boolean isSecondLetterVowelCompositePart = name.matches("^(?ui:.[эоуа]).*");
+        boolean isSecondLetterVowelComposite = name.matches("^(?ui:.[еёюя]).*");
+
+        boolean isThirdLetterVowel = name.matches("^(?ui:..[аиоуыэеёюя]).*");
+        boolean isThirdLetterVowelCompositePart = name.matches("^(?ui:..[эоуа]).*");
+        boolean isThirdLetterVowelComposite = name.matches("^(?ui:..[еёюя]).*");
+
+        if (name.equals(PREFIX)) {
+            return PREFIX;
+        }
+        if (name.length() == 1 && isFirstLetterVowelCompositePart) {
+            return PREFIX.substring(0, 2).concat(compositePartVowelToFullComposite(name.charAt(0)));
+        } else if (isFirstLetterVowelComposite) {
+            return PREFIX.substring(0, 2).concat(name);
+        } else if (name.length() == 1) {
+            return name;
+        }
+
+        if (isSecondLetterShortI) {
+            return PREFIX_EXCEPTION.concat(name.substring(2));
+        } else if (isFirstLetterVowel && isFirstLetterVowelCompositePart) {
+            return PREFIX.substring(0, 2).concat(compositePartVowelToFullComposite(name.charAt(0)) + name.substring(1));
+        }else if (isFirstLetterVowel) {
+            return PREFIX.substring(0, 2).concat(name);
+        } else if (isSecondLetterVowel && isSecondLetterVowelCompositePart) {
+            return PREFIX.substring(0, 2).concat(compositePartVowelToFullComposite(name.charAt(1)) + name.substring(2));
+        } else if (isSecondLetterVowel && isSecondLetterVowelComposite) {
+            return PREFIX.substring(0, 2).concat(name.substring(1));
+        } if (isSecondLetterVowel) {
+            return PREFIX.concat(name.substring(2));
+        } else if (isThirdLetterVowel && isThirdLetterVowelCompositePart) {
+            return PREFIX.substring(0, 2).concat(compositePartVowelToFullComposite(name.charAt(2))).concat(name.substring(3));
+        } else if (isThirdLetterVowel) {
+            return PREFIX.substring(0, 2).concat(String.valueOf(name.charAt(2))).concat(name.substring(3));
+        } else {
+            return PREFIX.concat(name.substring(1));
+        }
+    }
+
+    public static String compositePartVowelToFullComposite(Character compositePart) {
+        switch (compositePart) {
+            case 'а': return "я";
+            case 'э': return "е";
+            case 'о': return "ё";
+            case 'у': return "ю";
+            default: return "";
+        }
+    }
+}
