f5a4006da9befbe53cf53e63e3e8de84200c281a
ksamorodov <kirill.samorodov@diginetica.com>
2024-03-16 01:08:29 +0700

    Quickfix anton exception

--- dickname-backend/src/main/java/com/ksamorodov/dickname/service/DickNameService.java
+++ dickname-backend/src/main/java/com/ksamorodov/dickname/service/DickNameService.java
@@ -2,13 +2,28 @@ package com.ksamorodov.dickname.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class DickNameService {
     public static final String PREFIX = "Хуи";
     public static final String PREFIX_EXCEPTION = "Хуй";
     public static final String PREFIX_EXCEPTION_ANTON = "Антон";
 
-    public String createDickName(String name) {
+    public String generateDickSentence(String sentence) {
+        String[] words = sentence.split("\\s+");
+
+        // Обрабатываем каждое слово с помощью createDickName
+        String[] dickNames = Arrays.stream(words)
+                .map(i -> createDickName(i))
+                .toArray(String[]::new);
+
+        // Объединяем обработанные слова обратно в строку
+        return String.join(" ", dickNames);
+
+    }
+
+    private String createDickName(String name) {
         name = name.toLowerCase();
 
         boolean isFirstLetterVowel = name.matches("^(?ui:[аеёиоуыэюя]).*");
@@ -24,15 +39,15 @@ public class DickNameService {
         boolean isThirdLetterVowelCompositePart = name.matches("^(?ui:..[эоуа]).*");
         boolean isThirdLetterVowelComposite = name.matches("^(?ui:..[еёюя]).*");
 
-        if (name.equals(PREFIX)) {
+        if (name.equals(PREFIX.toLowerCase())) {
             return PREFIX;
         }
 
-        if (name.equals(PREFIX_EXCEPTION)) {
+        if (name.equals(PREFIX_EXCEPTION.toLowerCase())) {
             return PREFIX_EXCEPTION;
         }
 
-        if (name.equals(PREFIX_EXCEPTION_ANTON)) {
+        if (name.equals(PREFIX_EXCEPTION_ANTON.toLowerCase())) {
             return "Гондон";
         }
 
