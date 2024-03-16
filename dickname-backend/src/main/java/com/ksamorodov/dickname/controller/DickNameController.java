a61da2b51c6cde4f7215705e21344b64cd407018
serje3 <serjerin322@gmail.com>
2024-03-16 18:50:33 +0300

    feat: batch request

--- dickname-backend/src/main/java/com/ksamorodov/dickname/controller/DickNameController.java
+++ dickname-backend/src/main/java/com/ksamorodov/dickname/controller/DickNameController.java
@@ -4,12 +4,15 @@ import com.ksamorodov.dickname.dao.request.GenerateDickNameRequest;
 import com.ksamorodov.dickname.dao.response.GenerateDickNameResponse;
 import com.ksamorodov.dickname.service.DickNameService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +23,11 @@ public class DickNameController {
     @CrossOrigin
     public GenerateDickNameResponse generateDickName(@Valid @RequestBody GenerateDickNameRequest dickNameRequest) {
         return new GenerateDickNameResponse(dickNameService.generateDickSentence(dickNameRequest.getName()));
+    }
+
+    @PostMapping("/generate/dickname/batch")
+    @CrossOrigin
+    public List<GenerateDickNameResponse> generateDickNames(@Valid @NotEmpty @RequestBody List<GenerateDickNameRequest> dickNameRequests) {
+        return dickNameRequests.stream().map(r -> new GenerateDickNameResponse(dickNameService.generateDickSentence(r.getName()))).toList();
     }
 }
